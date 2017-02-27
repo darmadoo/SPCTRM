00980efcb8461c055c789759641c3c977e9a9487
darmadoo <markdarmadi@gmail.com>
2017-02-26 21:33:12 -0800

    Cleaned up JS

--- js/app.js
+++ js/app.js
@@ -1,58 +1,71 @@
-var box = [];
-var elBox = [];
-var elText = [];
+(function(){
+	'use strict';
+	var box = [];
+	// Get the array of slots 
+	var slots = document.getElementsByClassName("box");
 
-function Slot(xLoc, yLoc){
-  this.picked = false;
-  this.colorHex = "THIS SHOULD NOT COME OUT";
-  this.changePicked = function(){
-  	this.picked = !this.picked;
-  };
-}
+	// Slot object
+	function Slot(){
+	  // If color is picked for that slot
+	  this.picked = false;
+	  // Should record the color that is picked
+	  this.colorHex = "THIS SHOULD NOT COME OUT";
+	  // Function to toggle the picked value
+	  this.changePicked = function(){
+	  	this.picked = !this.picked;
+	  };
+	}
 
-// on load
-document.addEventListener('DOMContentLoaded', function () {
-	for(i = 0; i < 5; i++){
-    	box[i] = new Slot();
-    	elBox[i] = document.getElementById("slot" + i);
-    	elText[i] = document.getElementById("text" + i);
-  	}
-});
-
-// Get the array of slots 
-var slots = document.getElementsByClassName("box");
-// Attach click listener
-for(var i = 0; i < slots.length; i++){
-	slots[i].addEventListener('click', function(){
-		var cur = this.id;
-		var curEl = box[cur[cur.length-1]];
-		curEl.changePicked();
-		update(cur[cur.length-1]);
+	// on load DOM
+	document.addEventListener('DOMContentLoaded', function () {
+		// Initalize the slots 
+		for(i = 0; i < slots.length; i++){
+	    	box[i] = new Slot();
+	  	}
 	});
-}
 
+	document.getElementById("clearButton").addEventListener('click', function(){
+		clearSlots();
+	});
 
-function update(n){
-	if(box[n].picked == true){
-	  elText[n].style.visibility = "hidden";
-	  elBox[n].style.backgroundColor = "rgb(22, 149, 138)";
-	  elBox[n].style.boxShadow = "0 0 0 black";
+	// Attach click listener to each slot
+	for(var i = 0; i < slots.length; i++){
+		slots[i].addEventListener('click', function(){
+			var cur = this.id;
+			var index = cur[cur.length-1];
+			// Toggle the status of the slot
+			box[index].changePicked();
+			// Update the color
+			update(index, this);
+		});
 	}
-	else if(box[n].picked == false) {
-	  elText[n].style.visibility = "visible";
-	  elBox[n].style.backgroundColor = "white";
-	  elBox[n].style.boxShadow = "inset 0px 0px 10px rgba(0, 0, 0, 0.2)";
+
+	// Function to update the color of the slot
+	// takes in the index of the slot and the HTML of the selected slot
+	function update(index, curSlot){
+		// curSlot.children[0] is the plus sign
+		if(box[index].picked){
+		  curSlot.children[0].style.visibility = "hidden";
+		  curSlot.style.backgroundColor = "rgb(22, 149, 138)";
+		  curSlot.style.boxShadow = "0 0 0 black";
+		}
+		else{
+		  curSlot.children[0].style.visibility = "visible";
+		  curSlot.style.backgroundColor = "white";
+		  curSlot.style.boxShadow = "inset 0px 0px 10px rgba(0, 0, 0, 0.2)";
+		}
 	}
-}
 
-function clearSlots(){
-  for(var n = 0; n < 5; n++){
-    box[n].picked = false;
-    update(n);
-  }
-  
-}
+	// Function to reset the color palette
+	function clearSlots(){
+	  // Loop through the array of slots and set the pick to false and update
+	  for(var n in box){
+	    box[n].picked = false;
+	    update(n, document.getElementById("slot" + n));
+	  }
+	  
+	}
 
-document.getElementById("clearButton").addEventListener('click', function(){
-	clearSlots();
-});
+	
+
+}());
